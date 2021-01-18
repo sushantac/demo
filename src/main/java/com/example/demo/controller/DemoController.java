1af301fa858563730fe6c48be710de7e11aca4f6
Sushant Chanmanwar <sushant.chanmanwar@gmail.com>
2021-01-18 14:04:23 +1100

    Sample java spring-boot app

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class DemoController {
+
+	@GetMapping(path = "test")
+	public String getTest() {
+		return "Testing the api deployment to azure...";
+	}
+}
